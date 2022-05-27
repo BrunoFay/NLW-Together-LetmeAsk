9a3440172a2a306a50af4622c2865fc61668aad1
BRUNO <brunofay1@hotmail.com>
2022-05-27 17:22:36 -0300

    FIX css responsive on rooms pages

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -18,7 +18,7 @@ export default function Question({
        ${isAnswered ? 'answered hover:ring-current' : ''}
       ${isDarkMode ? 'bg-slate-100' : 'bg-[#fefefe]'} dark:bg-slate-100
     peer-last:bg-black hover:ring-2 hover:ring-mainColor-500 transition-shadow flex flex-col gap-4 justify-between
-      md:w-[55vw]  w-[90vw] rounded-lg p-5 `}>
+      lg:w-[55vw]  w-[90vw] rounded-lg p-5 `}>
       <p className='font-[Roboto] text-base  max-h-[6rem] text-[#29292e]  break-all overflow-auto w-auto  '>{content}
       </p>
       <footer className='flex justify-between gap-2'>
